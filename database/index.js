5f175d6e5f93b62ce67f9902ed1e927602c1d622
zamzoum94 <mzemzemi9@gmail.com>
2020-05-26 20:57:05 +0100

    user model

--- database/index.js
+++ database/index.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
-const config = require('./database.config')
-const driver = require('./models/driver')
+const config = require('./database.config');
+const driver = require('./models/driver');
+const user = require('./models/user');
+
 const sql = new Sequelize(config.database_name, config.username, config.password, {
     dialect: config.dialect
 });
-driver(sql, Sequelize)
+
+driver(sql, Sequelize);
+user(sql, Sequelize);
