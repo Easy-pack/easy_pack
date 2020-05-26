042a34d567fbd1cc47f9ac33dd818beec0e21166
zamzoum94 <mzemzemi9@gmail.com>
2020-05-26 19:48:46 +0100

    fixing init

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('./routes');
+
 app.use(cors())
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-const routes = require('./routes')
-app.use('/auth', routes.auth)
+
+app.use('/auth', routes.auth);
 
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
