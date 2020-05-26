07ddfdb524e644df3d1b4322c160860ccc8c41f5
maminez23 <60395940+maminez23@users.noreply.github.com>
2020-05-26 22:29:39 +0200

    database models

--- database/models/vehicles.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sql,  Sequelize) =>{
-    const vehicles = sql.define({
-        type: Sequelize.STRING,
-        license_plate : Sequelize.STRING,
-        Reg_number: Sequelize.NUMBER,
-    })
-};
-
