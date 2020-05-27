0f476a69e75dd43dc1e9366c3bfa0383a0ac9304
unknown <nouhachorfi2@gmail.com>
2020-05-27 22:38:45 +0100

    auth driver component

---
+++ client/src/app/components/auth/sign-up-driver/sign-up-driver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-driver',
+  templateUrl: './sign-up-driver.component.html',
+  styleUrls: ['./sign-up-driver.component.css']
+})
+export class SignUpDriverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
